fcb2af5acf81b0adf1b0f5058aa97bbe564fd56c
Patrick <st10497579@rcconnect.edu.za>
2026-08-27 10:08:24 +0200

    Created Patient class
    Stores patient information, including ID, name, age, gender, medical condition, and category. Included getters for each field and a displayDetails() method for printing patient information.

---
+++ Patient.java
@@ -0,0 +1,12 @@
+package hospital;
+
+public class Patient {
+    // Fields (private for information hiding)
+    private String patientID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String medicalCondition;
+    private PatientCategory category;
+}
